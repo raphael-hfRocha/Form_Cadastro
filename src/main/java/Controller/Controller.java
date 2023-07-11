19778b84b2e94f8d50a1320e72289a45ca4305e4
Raphael <raphaelrochaacft@gmail.com>
2023-07-11 13:37:45 -0300

    CREATE concluído

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -8,15 +8,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import Model.DAO;
+import Model.JavaBeans;
 
 /**
  * Servlet implementation class Controller
  */
-@WebServlet(urlPatterns = { "/Controller", "/main" })
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	DAO dao = new DAO();
+	JavaBeans cadastro = new JavaBeans();
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -36,6 +38,10 @@ public class Controller extends HttpServlet {
 		System.out.println(action);
 		if (action.equals("/main")) {
 			usuarios(request, response);
+		} else if (action.equals("/insert")) {
+			novoUsuario(request, response);
+		} else {
+			response.sendRedirect("index.html");
 		}
 	}
 
@@ -43,5 +49,27 @@ public class Controller extends HttpServlet {
 	protected void usuarios(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.sendRedirect("Registro.jsp");
+	}
+
+	// Novo usuario
+	protected void novoUsuario(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+//			//	Teste de recebimento dos dados do formulário			
+//			System.out.println(request.getParameter("nome"));
+//			System.out.println(request.getParameter("sobrenome"));
+//			System.out.println(request.getParameter("email"));
+//			System.out.println(request.getParameter("senha"));
+
+		// Setando as variávieis JavaBeans
+		cadastro.setNome(request.getParameter("nome"));
+		cadastro.setSobrenome(request.getParameter("sobrenome"));
+		cadastro.setEmail(request.getParameter("email"));
+		cadastro.setSenha(request.getParameter("senha"));
+		
+		dao.inserirUsuario(cadastro);
+		
+		
+		response.sendRedirect("main");
 	}
 }
--- src/main/java/Model/DAO.java
+++ src/main/java/Model/DAO.java
@@ -2,36 +2,34 @@ package Model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class DAO {
-	/*	Módulos de conexão */
-	
-	//	Parâmetros de conexão
-	
+	/* Módulos de conexão */
+
+	// Parâmetros de conexão
+
 	private String driver = "com.mysql.cj.jdbc.Driver";
 	private String url = "jdbc:mysql://localhost:3306/form_Cadastro?useTimeZone=true&serverTimeZone=UTC";
 	private String user = "root";
 	private String password = "Raphael#24";
-	
-	
-	//	Métodos de conexão
-	
-	private Connection conectar()
-	{
+
+	// Métodos de conexão
+
+	private Connection conectar() {
 		Connection con = null;
 		try {
 			Class.forName(driver);
 			con = DriverManager.getConnection(url, user, password);
 			return con;
-		} catch (Exception e) 
-		{
+		} catch (Exception e) {
 			System.out.println(e);
 			return null;
 		}
 	}
-	
+
 //	Teste de conexão
-	
+
 //	public void testeConexao()
 //	{
 //		try {
@@ -42,4 +40,30 @@ public class DAO {
 //			System.out.println(e);
 //		}
 //	}
+
+	/* Método CREATE/CRUD */
+
+	public void inserirUsuario(JavaBeans cadastro) {
+		String create = "insert into Cadastro(nome, sobrenome, email, senha) values (?, ?, ?, ?)";
+		try {
+			Connection con = conectar();
+
+			PreparedStatement pst = con.prepareStatement(create);
+
+			pst.setString(1, cadastro.getNome());
+			pst.setString(2, cadastro.getSobrenome());
+			pst.setString(3, cadastro.getEmail());
+			pst.setString(4, cadastro.getSenha());
+
+			// Executando a query
+
+			pst.executeUpdate();
+
+			// Encerrando a conexão com o banco
+
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
